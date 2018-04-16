8a6ab2c31c5f45fb1032facae7d465a683a496f5
dem0sequence <dem0sequence@yahoo.com>
2018-04-17 00:41:20 +0300

    removed resources/views/vendor from gitignore to keep tracking backpack sidebar

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,9 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+<li><a href="{{ backpack_url('users') }}"><i class="fa fa-user"></i> <span>Users</span></a></li>
+<li><a href="{{ backpack_url('roles') }}"><i class="fa fa-server"></i> <span>User roles</span></a></li>
+<li><a href="{{ backpack_url('groups') }}"><i class="fa fa-users"></i> <span>Groups</span></a></li>
+<li><a href="{{ backpack_url('allergies') }}"><i class="fa fa-tencent-weibo"></i> <span>Allergies</span></a></li>
+<li><a href="{{ backpack_url('allergens') }}"><i class="fa fa-bug"></i> <span>Allergens</span></a></li>
+<li><a href="{{ backpack_url('statistics') }}"><i class="fa fa-users"></i> <span>User statistics</span></a></li>
+<li><a href="{{ backpack_url('statistics_types') }}"><i class="fa fa-pie-chart"></i> <span>Statistics Types</span></a></li>
